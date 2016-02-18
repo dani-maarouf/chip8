6da21e39c40ecc1111c5c849e03d16fcd2998d24
Dani Maarouf <dmaarouf@mail.uoguelph.ca>
2016-02-17 23:43:31 -0500

    Reading in hex file

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC=gcc
-CFLAGS=-Iinclude -std=c99 -Wall
+CFLAGS=-Iinclude -std=c99 -Wall -g
 
 chip8: src/main.c
 	$(CC) $(CFLAGS) src/main.c -o bin/chip8
--- src/main.c
+++ src/main.c
@@ -1,8 +1,69 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct ROM {
+    int size;
+    int * intArray;
+} ROM;
+
+ROM readFile(const char * fileLocation) {
+
+    ROM newROM;
+    newROM.intArray = NULL;
+    newROM.size = 0;
+
+    if (fileLocation == NULL) {
+        perror("readFile() -> Error: File location string is NULL\n");
+        return newROM;
+    }
+
+    FILE * romFile;
+    romFile = fopen(fileLocation, "rb");
+    if (romFile == NULL) {
+        perror("readFile() -> Error: Could not open ROM file\n");
+        return newROM;
+    }
+
+    int readChar;
+    while ((readChar = fgetc(romFile)) != EOF) {
+        newROM.size++;
+    }
+
+    fclose(romFile);
+    romFile = fopen(fileLocation, "rb");
+    if (romFile == NULL) {
+        perror("readFile() -> Error: Could not open ROM file\n");
+        return newROM;
+    }
+
+    newROM.intArray = malloc(sizeof(int) * newROM.size);
+
+    for (int x = 0; x < newROM.size; x++) {
+        newROM.intArray[x] = fgetc(romFile);
+    }
+
+    return newROM;
+}
 
 int main (int argc, char * argv[]) {
 
-    printf("Hello world\n");
+    if (argc != 2) {
+        perror("Please call program with 1 argument pointing to location of ROM\n");
+        return -1;
+    }
 
-    return 1;
+    ROM game;
+    game = readFile(argv[1]);
+    if (game.intArray == NULL) {
+        perror("main() -> Error: Could not read ROM\n");
+        return -1;
+    }
+
+    for (int x = 0; x < game.size; x++) {
+        printf("%x ", game.intArray[x]);
+    }
+
+
+    printf("\nSuccess\n");
+    return 0;
 }
